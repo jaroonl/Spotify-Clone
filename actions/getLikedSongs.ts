719255a93c0f5e99e0fc4e612eb4945a1c4b0fc7
jaroonl <jaronslin@gmail.com>
2023-09-21 13:49:48 -0700

    finished favorite functionality

---
+++ actions/getLikedSongs.ts
@@ -0,0 +1,39 @@
+import { Song } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getLikedSongs = async (): Promise<Song[]> => {
+  const supabase = createServerComponentClient({
+    cookies: cookies,
+  });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const { data, error } = await supabase
+    .from("liked_songs")
+    .select("*, songs(*)")
+    //2nd key songs has a relation to public songs id
+    //want to fetch the entire song we aiming at
+    .eq("user_id", session?.user?.id)
+    //fetch things only from currently logged in users
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error);
+    return [];
+  }
+
+  if (!data) {
+    return [];
+  }
+
+  return data.map((item) => ({
+    ...item.songs,
+  }));
+  //spreading a relation that we have populated with songs(*)
+  //in each of the fields
+};
+
+export default getLikedSongs;
